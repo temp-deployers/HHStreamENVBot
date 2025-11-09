95a0752f19b3edb60a45cda2a6fd87e473324310
Parveen Bhadoo <github-notify@hashhackers.com>
2025-11-09 21:09:17 +0530

    sync

--- WebStreamer/server/__init__.py
+++ WebStreamer/server/__init__.py
@@ -1,13 +1,13 @@
 # Taken from megadlbot_oss <https://github.com/eyaadh/megadlbot_oss/blob/master/mega/webserver/__init__.py>
 # Thanks to Eyaadh <https://github.com/eyaadh>
-# Enhanced by Hash Hackers & LiquidX Projects
+# This file is a part of TG-
+# Coding : Jyothis Jayanth [@EverythingSuckz]
 
 from aiohttp import web
-from .stream_routes_v2 import routes
+from .stream_routes import routes
 
 
 def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-
--- WebStreamer/utils/cryptography.py
+++ WebStreamer/utils/cryptography.py
@@ -1,14 +1,12 @@
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad, pad
+from Crypto.Util.Padding import unpad
 
 import base64
 from hashlib import sha256
-from ..vars import Var
 
-# Get encryption keys from environment
-SECRET_KEY = Var.SECRET_KEY
-key = Var.AES_KEY
-iv = Var.AES_IV.encode('utf-8')
+key = 'BHADOO9854752658'
+iv =  'CLOUD54158954721'.encode('utf-8')
+SECRET_KEY = '647e2c1ac884418b5c270862a9a484105e88b11f097fa9d5ddd09eb4c53737bd'
 
 def verify_sha256_key(cid, fid, expiration_time, sha256_key):
     try:
@@ -30,29 +28,3 @@ def decrypt(enc, key, iv):
     decrypted_str = decrypted.decode('utf-8')
     channel_id, message_id, expiration_time = decrypted_str.split('|')
     return channel_id, message_id, int(expiration_time)
-
-def encrypt_channel_id(channel_id: int) -> str:
-    """Encrypt channel ID for use in links"""
-    try:
-        data = str(channel_id).encode('utf-8')
-        cipher = AES.new(key.encode('utf-8'), AES.MODE_CBC, iv)
-        encrypted = cipher.encrypt(pad(data, 16))
-        return base64.b64encode(encrypted).decode('utf-8')
-    except Exception as e:
-        print(f"Encryption error: {e}")
-        return str(channel_id)
-
-def decrypt_channel_id(encrypted_id: str) -> int:
-    """Decrypt channel ID from link"""
-    try:
-        enc = base64.b64decode(encrypted_id)
-        cipher = AES.new(key.encode('utf-8'), AES.MODE_CBC, iv)
-        decrypted = unpad(cipher.decrypt(enc), 16)
-        return int(decrypted.decode('utf-8'))
-    except Exception as e:
-        print(f"Decryption error: {e}")
-        # Try to parse as direct channel ID (backwards compatibility)
-        try:
-            return int(encrypted_id)
-        except:
-            return 0
